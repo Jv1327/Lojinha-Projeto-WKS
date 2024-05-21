4dd38b2f60879f2b51583be605312bb1572aefc6
Jv1327 <jvrodrigues132770@gmail.com>
2024-05-21 19:53:39 -0300

    first commit

---
+++ src/components/buttons/cart-button/styled.ts
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+
+export const Button = styled.button`
+    background-color: var(--bg-color-01);
+    padding: .5em .75em;
+    border-radius: 8px;
+    user-select: none;
+    border: none;
+
+    font-family: "Montserrat", sans-serif;
+    font-weight: 700;
+    font-size: 16px;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: .75em;
+
+    &:hover
+    {
+        cursor: pointer;
+        background-color: var(--bg-color-02);
+    }
+`;
